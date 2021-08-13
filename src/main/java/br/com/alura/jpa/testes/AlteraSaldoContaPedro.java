3225b921140033a8fd6e46332d7eef6453d458c9
Pedro Arthur Barbosa Carvalho <pedro.barbosa@zup.com.br>
2021-08-13 15:38:57 -0300

    entity manager cria e atualiza entidades

---
+++ src/main/java/br/com/alura/jpa/testes/AlteraSaldoContaPedro.java
@@ -0,0 +1,23 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Conta;
+
+public class AlteraSaldoContaPedro {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager();
+		
+		Conta contaDoPedro = em.find(Conta.class, 1);
+		
+		em.getTransaction().begin();
+		contaDoPedro.setSaldo(20.0);
+		em.getTransaction().commit();
+	}
+
+}
